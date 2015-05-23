ee8b13b2649ec28ba9873f11549f49c07b2235d3
urthbound <jeffowler@gmail.com>
2015-05-23 17:02:29 -0400

    simpe math works

--- parsing.c
+++ parsing.c
@@ -3,6 +3,33 @@
 #include <editline/readline.h>
 #include "mpc.h"
 
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    /* If tagged as number return it directly. */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* The operator is always second child. */
+    char* op = t->children[1]->contents;
+    long x = eval(t->children[2]);
+
+    /* Iterate the remaining children and combining */
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
 
 int main(int argc, char** argv) {
 
@@ -31,21 +58,22 @@ int main(int argc, char** argv) {
         /* Attempt to Parse the user Input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-              /* On Success Print the AST */
-              mpc_ast_print(r.output);
-                mpc_ast_delete(r.output);
+            /* On Success Print the AST */
+            /* mpc_ast_print(r.output); */
+            long result = eval(r.output);
+            printf("%li\n", result);
+            mpc_ast_delete(r.output);
         } else {
-              /* Otherwise Print the Error */
-              mpc_err_print(r.error);
-                mpc_err_delete(r.error);
+            /* Otherwise Print the Error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
         }
-
 
         free(input);
     }
 
     /* Undefine and Delete our Parsers */
-    /* mpc_cleanup(4, Number, Operator, Expr, Lispy); */
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
